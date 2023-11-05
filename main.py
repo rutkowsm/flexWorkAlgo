883abccd35bbec3792a8bc925b91accb78661f53
marcin <s12497@pjwstk.edu.pl>
2023-11-05 23:38:11 +0100

    Print mod

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import itertools
 
-
+DAYS_OF_WEEK = {
+    0: "Monday",
+    1: "Tuesday",
+    2: "Wednesday",
+    3: "Thursday",
+    4: "Friday",
+    5: "Saturday",
+    6: "Sunday"
+}
 class Employee:
     def __init__(self, name, email, personal_calendar=None, min_weekly_hours=0, max_weekly_hours=100,
                  min_shift_length=1, max_shift_length=12):
@@ -33,7 +41,7 @@ class Vacancy:
         self.day = day
         self.start_time = start_time
         self.end_time = end_time
-        self.blocks = ['EMPTY'] * (end_time - start_time)
+        self.blocks = ['EMPTY'] * (end_time - start_time + 1)
 
 
 class Restaurant:
@@ -44,33 +52,6 @@ class Restaurant:
     def add_vacancy(self, day, start_time, end_time):
         self.restaurant_calendar[day].append(Vacancy(day, start_time, end_time))
 
-
-# def schedule_employees(employees, restaurant):
-#     # For each day in the restaurant calendar
-#     for day_index, vacancies in enumerate(restaurant.restaurant_calendar):
-#         # For each vacancy in the day
-#         for vacancy in vacancies:
-#             # Attempt to schedule each block in the vacancy
-#             for block_index in range(len(vacancy.blocks)):
-#                 scheduled_employee = None
-#                 for employee in employees:
-#                     hour_of_day = (vacancy.start_time + block_index) % 24
-#                     # Check if the employee is available and has not exceeded max weekly hours
-#                     if (employee.personal_calendar[day_index][hour_of_day] and
-#                             employee.hours_scheduled < employee.max_weekly_hours):
-#                         # Check for min_shift_length constraint
-#                         if can_schedule_shift(employee, day_index, hour_of_day, vacancy.start_time, vacancy.end_time,
-#                                               block_index):
-#                             vacancy.blocks[block_index] = employee.name
-#                             employee.personal_calendar[day_index][hour_of_day] = False
-#                             employee.hours_scheduled += 1
-#                             scheduled_employee = employee
-#                             break  # Stop searching if we scheduled someone
-#                 # If the block remains empty after trying all employees, mark it as EMPTY
-#                 if not scheduled_employee:
-#                     vacancy.blocks[block_index] = 'EMPTY'
-#
-#     return restaurant
 
 def is_employee_available(employee, day_index, start_time, end_time):
     # Check if the employee is available for the entire duration from start_time to end_time
@@ -186,7 +167,9 @@ def swap_shift(current_employee, new_employee, day_index, block_index, vacancy):
     # Perform the shift swap
     for hour in range(block_index, block_index + current_employee.min_shift_length):
         # Update the vacancy schedule
-        vacancy.blocks[hour] = new_employee.name
+        index = hour - vacancy.start_time  # Calculate the correct index for vacancy.blocks
+        if 0 <= index < len(vacancy.blocks):
+            vacancy.blocks[index] = new_employee.name
         # Update the personal calendars
         current_employee.personal_calendar[day_index][vacancy.start_time + hour] = True
         new_employee.personal_calendar[day_index][vacancy.start_time + hour] = False
@@ -196,26 +179,44 @@ def swap_shift(current_employee, new_employee, day_index, block_index, vacancy):
     new_employee.hours_scheduled += current_employee.min_shift_length
 
 
+
 # Example usage:
 john_calendar = [[0, 8, 11], [1, 15, 18]]  # John zajęty między 15 a 18 we wtorek
 kate_calendar = []  # Kate is available at all times
+bill_calendar = []  # Kate is available at all times
 
 employee_1 = Employee(name="John", email="john@mail.com", personal_calendar=john_calendar, min_shift_length=2,
                       max_shift_length=8)
 employee_2 = Employee(name="Kate", email="kate@mail.com", personal_calendar=kate_calendar, min_shift_length=4,
                       max_shift_length=10)
+employee_3 = Employee(name="Bill", email="bill@mail.com", personal_calendar=bill_calendar, min_shift_length=4,
+                      max_shift_length=10)
 
-employees = [employee_1, employee_2]
+employees = [employee_1, employee_2, employee_3]
 
 restaurant = Restaurant("Bar Mleczny Apis")
-restaurant.add_vacancy(0, 8, 16)  # Add a vacancy on Monday from 8:00 to 16:00
-restaurant.add_vacancy(1, 10, 17)  # Add vacancy on Tuesday from 10:00 to 17:00
-restaurant.add_vacancy(1, 12, 16)  # Add vacancy on Tuesday from 12:00 to 16:00
+restaurant.add_vacancy(1, 8, 16)  # Add a vacancy on Tue from 8:00 to 16:00
+restaurant.add_vacancy(2, 10, 17)  # Add vacancy on Wed from 10:00 to 17:00
+restaurant.add_vacancy(3, 10, 18)  # Add vacancy on Thu from 12:00 to 16:00
+restaurant.add_vacancy(4, 10, 18)  # Add vacancy on Fri from 12:00 to 16:00
+restaurant.add_vacancy(4, 12, 18)  # Add vacancy on Fri from 12:00 to 16:00
+restaurant.add_vacancy(5, 10, 18)  # Add vacancy on Sat from 12:00 to 16:00
+restaurant.add_vacancy(5, 12, 18)  # Add vacancy on Sat from 12:00 to 16:00
+restaurant.add_vacancy(5, 12, 16)  # Add vacancy on Sat from 12:00 to 16:00
+restaurant.add_vacancy(6, 10, 18)  # Add vacancy on Sat from 12:00 to 16:00
+restaurant.add_vacancy(6, 12, 18)  # Add vacancy on Sat from 12:00 to 16:00
 
 restaurant_schedule = schedule_employees(employees, restaurant)
 
 # Display the schedule
 for day_index, vacancies in enumerate(restaurant_schedule.restaurant_calendar):
-    print(f"Day {day_index} (0=Monday, 6=Sunday):")
-    for vacancy in vacancies:
-        print(f"  From {vacancy.start_time} to {vacancy.end_time}: {vacancy.blocks}")
+    day_of_week = DAYS_OF_WEEK[day_index]
+    print(f"Day {day_index} - {day_of_week}:")
+
+    # Check if there are any vacancies for this day
+    if vacancies:
+        for vacancy in vacancies:
+            print(
+                f"[{', '.join([f'{hour}: {name}' for hour, name in enumerate(vacancy.blocks, start=vacancy.start_time)])}]")
+    else:
+        print(f"There is no vacancy set for {day_of_week}")
